d2d1992b7e5f022f27c3d2949587383d337873fe
jonahuque <jonahuque@gmail.com>
2026-01-12 17:30:56 +0100

    Implementando mostrar estado del tamagochi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,9 @@ package org.example;
 import java.util.Scanner;
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
+    private static int saciedad = 6;
+    private static int energia = 6;
+    private static int diversion = 6;
     public static void main(String[] args) {
         int opcion;
 
@@ -32,6 +35,35 @@ public class Main {
         }while(opcion < 0 || opcion > 4);
 
         return opcion;
+    }
+
+    public static void mostrarEstado(){
+        if (saciedad <=0 || energia <=0 || diversion <=0){
+            System.out.println(" (X_X)  :(Game Over):");
+            System.out.println(" /|_|\\");
+            System.out.println("  | |");
+            return;
+        }
+        if (saciedad > 5 && energia > 5 && diversion > 5){
+            System.out.println(" (•‿•)  ¡Estoy Feliz!");
+            System.out.println(" /|_|\\");
+            System.out.println("  | |");
+
+        }else if(diversion <=4){
+            System.out.println(" (-︵-)  Estoy aburrido");
+            System.out.println(" /|_|\\");
+            System.out.println("  | |");
+
+        }else if(energia <=4){
+            System.out.println(" (-_-)  ZZzzzZZZzzz");
+            System.out.println(" /|_|\\");
+            System.out.println("  | |");
+
+        }else if(saciedad <=4){
+            System.out.println(" (•︵•)  Tengo hambre!sd");
+            System.out.println(" /|_|\\");
+            System.out.println("  | |");
+        }
     }
 }
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
